f3b17e8ea81f59b78d0043376681c22201ebfecc
Alexandr Chizhov <a1ex.chizhov@yandex.ru>
2023-07-10 22:59:59 +0700

    Урок 5

--- online_shop/app_online_shop/views.py
+++ online_shop/app_online_shop/views.py
@@ -4,5 +4,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+# Функция, отображающая index.html
 def index(request):
-    return HttpResponse('Успешно!')
+    return render(request, 'index.html')
+# Функция, отображающая top-sellers.html
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
