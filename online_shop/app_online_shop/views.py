196c65a4937d594a14e56af7a74759583993c037
Alexandr Chizhov <a1ex.chizhov@yandex.ru>
2023-07-11 22:09:00 +0700

    ДЗ 4 к уроку 5 модуля 4

--- online_shop/app_online_shop/views.py
+++ online_shop/app_online_shop/views.py
@@ -7,6 +7,22 @@ from django.http import HttpResponse
 # Функция, отображающая index.html
 def index(request):
     return render(request, 'index.html')
+
 # Функция, отображающая top-sellers.html
 def top_sellers(request):
     return render(request, 'top-sellers.html')
+
+def advertisment_post(request):
+    return render(request, 'advertisement-post.html')
+
+def advertisment(request):
+    return render(request, 'advertisement.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def profile(request):
+    return render(request, 'profile.html')
+
+def register(request):
+    return render(request, 'register.html')
